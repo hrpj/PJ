ff1ae78b4a31a7760ccba0b153cda2c13fdb5433
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-26 18:55:17 +0700

    Update StaffInfor-02.php

--- HR/StaffInfor-02.php
+++ HR/StaffInfor-02.php
@@ -4,14 +4,14 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Staff Information Manage</title>
+    <title>Information Myself</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="font.css" rel="stylesheet">
+    <link href="styleinforHR.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display&display=swap" rel="stylesheet">
 
-	<!-- Nav Bar -->
+    <!-- Nav Bar -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<font color="#FFFFFF" size="5"> <i class="far fa-building"></i></font>
 		<a class="navbar-brand" href="http://localhost/HRPJ/HR/WelcomeSignoutForHR.php">&nbsp;ILoveDB Company</a>
@@ -86,20 +86,76 @@
 	</nav>
 </head>
 
+
   <body>
-      <br>
-      <h2>&nbsp;&nbsp;&nbsp;Staff Information Manage</h2>
-      <hr>
-      <!-- Search -->
-      <form class="form-inline my-2 my-lg-0">
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Staff ID&nbsp;&nbsp;
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="window.location.href = 'StaffInfor.html';">Search</button>
-    </form>
-    <!-- End Search -->
-    <div class="Searching">
-      <i><font color = "#D3D3D3"> Search Staff ID that you want to look information. </font></i>
-    </div>
+      <div class = "StaffInfor"><h3>Staff Information</h></div>
+      <div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
+      <!-- Information -->
+      <div class="Infor"><i class="fas fa-address-card"></i>Staff ID : HR031003<br><br><i class="fas fa-file-signature"></i>Name : Muchchahuakuy Sittiwong<br><br><i class="fas fa-venus-mars"></i>Gender : Male<br><br><i class="fas fa-birthday-cake"></i>Date of birth : 01-12-1998
+<br><br><i class="fas fa-hourglass-start"></i>Start Date : 01-01-2018<br><br>Check behavior point : <a href="CompetenceforHR.html" class="button-link">Click here!</a></div>
+      <div class="Infor1"><br><br><i class="fas fa-layer-group"></i>Department : Human Resource Management<br><br><i class="fas fa-briefcase"></i>Position : Manager<br><br><i class="fas fa-map-marker-alt"></i></i>Branch : Bang Mod <br><br><i class="fas fa-mobile-alt"></i>Mobilephone No. : 0984750369
+      <br><br><i class="fas fa-building"></i>Address : Cosmo Condo<br><br><i class="fas fa-money-check-alt"></i>Bank Account : 008-8-56155-4</div>
+      <!-- End Information -->
+      <!-- Table Work History -->
+      <div class = "WorkHis"><h4>Work History</h></div>
+          <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Company</th>
+      <th scope="col">Start Date</th>
+      <th scope="col">End Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Intel</td>
+      <td>01-03-2014</td>
+      <td>01-03-2015</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Central Group</td>
+      <td>01-06-2016</td>
+      <td>01-12-2017</td>
+    </tr>
+  </tbody>
+</table>
+<div class = "Graduate"><h4>Graduate History</h></div>
+    <table class="table">
+<thead class="thead-dark">
+<tr>
+<th scope="col">#</th>
+<th scope="col">University</th>
+<th scope="col">Field</th>
+<th scope="col">Degree</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<th scope="row">1</th>
+<td>Chulalongkorn University</td>
+<td>Engineering</td>
+<td>Bachelor</td>
+</tr>
+<tr>
+<th scope="row">2</th>
+<td>Mahidol University</td>
+<td>Marketing</td>
+<td>Doctor</td>
+</tr>
+</tbody>
+</table>
+<!-- End Table Work History -->
+<table class="thebuttons">
+    <tr><td>
+    <button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'StaffInforEdit.html';">Edit</button>
+</td><td>
+    <span><button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'SearchInforStaff.html';">Back</button></span>
+</td></tr>
+</table>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
