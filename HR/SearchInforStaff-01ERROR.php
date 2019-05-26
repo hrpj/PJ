58c558507d9262bcab3650e3f899b4e8babb0951
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-26 18:55:15 +0700

    Update SearchInforStaff-01ERROR.php

--- HR/SearchInforStaff-01ERROR.php
+++ HR/SearchInforStaff-01ERROR.php
@@ -91,24 +91,30 @@
 	</div>
 </head>
 
-  <body>
-      <br>
-      <h2>&nbsp;&nbsp;&nbsp;Staff Information Manage</h2>
-      <hr>
-      <!-- Search -->
-      <form class="form-inline my-2 my-lg-0">
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Staff ID&nbsp;&nbsp;
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="window.location.href = 'StaffInfor.html';">Search</button>
+<body>
+    <br>
+    <h2>&nbsp;&nbsp;&nbsp;Staff Information Manage</h2>
+    <hr>
+	  
+    <!-- Search -->
+    <form action="search2.php" method="POST" class="form-inline my-2 my-lg-0">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Staff ID&nbsp;&nbsp;
+		<input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search">
+		<button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="window.location.href = '#';">Search</button>
     </form>
+	
+	
+	
+	
     <!-- End Search -->
     <div class="Searching">
-      <i><font color = "#D3D3D3"> Search Staff ID that you want to look information. </font></i>
+		<i><font color = "#D3D3D3"> Search Staff ID that you want to look information. </font></i>
     </div>
+	
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
