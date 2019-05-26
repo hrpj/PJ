ecb5b11c8ca319e9feac8e14d12537580f2f46cb
Charoenboon <lazykiller555@hotmail.com>
2019-05-27 01:15:31 +0700

    Update StaffInforEditAction.php

--- HR/StaffInforEditAction.php
+++ HR/StaffInforEditAction.php
@@ -431,38 +431,10 @@ session_start();
 	echo $staffName;
 	echo $gender;
 	echo $start;
-	echo " ".$mobilePhoneNo;
-	echo " ".$staffAddress;
-	echo " ",$i." ".$j;
-	//$result = mysqli_query($con,"SELECT * FROM staff WHERE staffId LIKE '$id'");
-	
-	//$count=$result->num_rows;
-	//if ((empty($count))) 
-	//{
-	//	header("Location: http://localhost/HRPJ/ErrorSignin.html");
-	//}
-	//echo SESSION_id();
-	//while ($row = mysqli_fetch_array($result))
-	//{
-	//	$_SESSION["ID"] = $row['staffID'];
-	//
-	//	if ($password == $row['password'])
-	//	{
-	//		if(strpos($row['positionID'],$hr)!==false)
-	//		{
-	//			header("Location: http://localhost/HRPJ/HR/WelcomeSignoutForHR.php");
-	//		}
-	//		else
-	//		{
-	//		header("Location: http://localhost/HRPJ/Staff/WelcomeSignoutForStaff.php");
-	//		}
-	//	}
-	//	else
-	//	{
-	//		header("Location: http://localhost/HRPJ/ErrorSignin.html");
-	//	}
-	//}
-	
+	echo $mobilePhoneNo;
+	echo $staffAddress;
+	echo $i." ".$j;
+
 	mysqli_close($con);
 	session_write_close();
 ?>
