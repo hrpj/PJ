26cc883eeb0f663ba9c1b2c7efb9efbe8857e55c
Charoenboon <lazykiller555@hotmail.com>
2019-05-26 19:27:39 +0700

    Update StaffInfor-02.php

--- HR/StaffInfor-02.php
+++ HR/StaffInfor-02.php
@@ -122,84 +122,122 @@ session_start();
             }
 ?>
 <body>
-      <div class = "StaffInfor"><h3>Staff Information</h></div>
-      <div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
-      <!-- Information -->
-      <div class="Infor"><i class="fas fa-address-card"></i>Staff ID : <?php echo "$search"; ?><br>
-	  <br><i class="fas fa-file-signature"></i>Name : <?php echo "$staffName"; ?><br>
-	  <br><i class="fas fa-venus-mars"></i>Gender : <?php echo "$gender"; ?><br>
-	  <br><i class="fas fa-birthday-cake"></i>Date of birth : <?php echo "$DOB"; ?><br>
-	  <br><i class="fas fa-hourglass-start"></i>Start Date : <?php echo "$startDate"; ?><br>
-	  <br>Check behavior point : <a href="CompetenceforHR.html" class="button-link">Click here!</a></div>
-      <div class="Infor1"><br>
-	  <br><i class="fas fa-layer-group"></i>Department : <?php echo "$departmentName"; ?><br>
-	  <br><i class="fas fa-briefcase"></i>Position : <?php echo "$positionName"; ?><br>
-	  <br><i class="fas fa-map-marker-alt"></i></i>Branch : <?php echo "$BranchName"; ?> <br>
-	  <br><i class="fas fa-mobile-alt"></i>Mobilephone No. : <?php echo "$telNOStaff"; ?><br>
-	  <br><i class="fas fa-building"></i>Address : <?php echo "$staffAddress"; ?><br>
-	  <br><i class="fas fa-money-check-alt"></i>Bank Account : <?php echo "$bankAccount"; ?></div>
-      <!-- End Information -->
+      	<div class = "StaffInfor"><h3>Staff Information</h></div>
+      	<div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
+      	<!-- Information -->
+      	<div class="Infor"><i class="fas fa-address-card"></i>Staff ID : <?php echo "$search"; ?><br>
+	  	<br><i class="fas fa-file-signature"></i>Name : <?php echo "$staffName"; ?><br>
+	  	<br><i class="fas fa-venus-mars"></i>Gender : <?php echo "$gender"; ?><br>
+	  	<br><i class="fas fa-birthday-cake"></i>Date of birth : <?php echo "$DOB"; ?><br>
+	  	<br><i class="fas fa-hourglass-start"></i>Start Date : <?php echo "$startDate"; ?><br>
+	  	<br>Check behavior point : <a href="CompetenceforHR.html" class="button-link">Click here!</a></div>
+      	<div class="Infor1"><br>
+	  	<br><i class="fas fa-layer-group"></i>Department : <?php echo "$departmentName"; ?><br>
+	  	<br><i class="fas fa-briefcase"></i>Position : <?php echo "$positionName"; ?><br>
+	  	<br><i class="fas fa-map-marker-alt"></i></i>Branch : <?php echo "$BranchName"; ?> <br>
+	  	<br><i class="fas fa-mobile-alt"></i>Mobilephone No. : <?php echo "$telNOStaff"; ?><br>
+	  	<br><i class="fas fa-building"></i>Address : <?php echo "$staffAddress"; ?><br>
+	  	<br><i class="fas fa-money-check-alt"></i>Bank Account : <?php echo "$bankAccount"; ?></div>
+      	<!-- End Information -->
 	  
 	  
-      <!-- Table Work History -->
-      <div class = "WorkHis"><h4>Work History</h></div>
-          <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Company</th>
-      <th scope="col">Start Date</th>
-      <th scope="col">End Date</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Intel</td>
-      <td>01-03-2014</td>
-      <td>01-03-2015</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Central Group</td>
-      <td>01-06-2016</td>
-      <td>01-12-2017</td>
-    </tr>
-  </tbody>
-</table>
-<div class = "Graduate"><h4>Graduate History</h></div>
-    <table class="table">
-<thead class="thead-dark">
-<tr>
-<th scope="col">#</th>
-<th scope="col">University</th>
-<th scope="col">Field</th>
-<th scope="col">Degree</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<th scope="row">1</th>
-<td>Chulalongkorn University</td>
-<td>Engineering</td>
-<td>Bachelor</td>
-</tr>
-<tr>
-<th scope="row">2</th>
-<td>Mahidol University</td>
-<td>Marketing</td>
-<td>Doctor</td>
-</tr>
-</tbody>
-</table>
-<!-- End Table Work History -->
-<table class="thebuttons">
-    <tr><td>
-    <button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'StaffInforEdit.html';">Edit</button>
-</td><td>
-    <span><button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'http://localhost/HRPJ/HR/SearchInforStaff-01.php';">Back</button></span>
-</td></tr>
-</table>
+      	<!-- Table Work History -->
+      	<div class = "WorkHis"><h4>Work History</h></div>
+        <table class="table">
+  			<thead class="thead-dark">
+			    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">Company</th>
+				      <th scope="col">Start Date</th>
+				      <th scope="col">End Date</th>
+			    </tr>
+			</thead>
+			<tbody>
+		    	<?php 
+	                $result = mysqli_query($con,"SELECT * FROM workinghistory WHERE staffId LIKE '$search'");
+	                $count=$result->num_rows;
+	                $i = 1;
+	                if ((empty($count))) 
+	                {
+	                    echo "<tr>";
+	                    echo "<th scope='row'>".$i."</th>";
+	                    echo "<td>No Data</td>";
+	                    echo "<td>----/--/--</td>";
+	                    echo "<td>----/--/--</td>";
+	                    echo "</tr> ";
+	                }
+	                else
+	                {
+	                    while ($row = mysqli_fetch_array($result))
+	                    {
+	                        $company = $row['company'];
+	                        $startDate = $row['startDate'];
+	                        $endDate = $row['endDate'];
+	                        echo "<tr>";
+	                        echo "<th scope='row'>".$i."</th>";
+	                        echo "<td>".$company."</td>";
+	                        echo "<td>".$startDate."</td>";
+	                        echo "<td>".$endDate."</td>";
+	                        echo "</tr> ";
+	                        $i++;
+	                    }
+	                }
+	            ?>
+		  	</tbody>
+		</table>
+		<div class = "Graduate"><h4>Graduate History</h></div>
+    	<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">University</th>
+					<th scope="col">Field</th>
+					<th scope="col">Degree</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+	                $result = mysqli_query($con,"SELECT * FROM education WHERE staffId LIKE '$search'");
+	                $count=$result->num_rows;
+	                $i = 1;
+	                if ((empty($count))) 
+	                {
+	                    echo "<tr>";
+	                    echo "<th scope='row'>".$i."</th>";
+	                    echo "<td>No Data</td>";
+	                    echo "<td>No Data</td>";
+	                    echo "<td>No Data</td>";
+	                    echo "</tr> ";
+	                }
+	                while ($row = mysqli_fetch_array($result))
+	                {
+	                    $university = $row['university'];
+	                    $field = $row['field'];
+	                    $degree = $row['degree'];
+	                    echo "<tr>";
+	                    echo "<th scope='row'>".$i."</th>";
+	                    echo "<td>".$university."</td>";
+	                    echo "<td>".$field."</td>";
+	                    echo "<td>".$degree."</td>";
+	                    echo "</tr> ";
+	                    $i++;
+	                }
+	            ?>
+			</tbody>
+		</table>
+		<!-- End Table Work History -->
+		<table class="thebuttons">
+		    <tr>
+		    	<td>
+		    		<button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'http://localhost/HRPJ/HR/StaffInforEdit-03.php';">Edit</button>
+				</td>
+				<td>
+				    <span>
+				    	<button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'http://localhost/HRPJ/HR/SearchInforStaff-01.php';">Back</button>
+				    </span>
+				</td>
+			</tr>
+		</table>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
