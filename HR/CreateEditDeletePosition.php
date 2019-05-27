aa29e7b0d78080b2be9b5a7c87f9c61f9abfd43b
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 00:24:56 +0700

    Update CreateEditDeletePosition.php

--- HR/CreateEditDeletePosition.php
+++ HR/CreateEditDeletePosition.php
@@ -13,26 +13,51 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$branchName = $_SESSION["BRANCH"];
+
 // Escape user inputs for security
 $positionName = mysqli_real_escape_string($conn, $_REQUEST['positionName']);
 $minSalary = mysqli_real_escape_string($conn, $_REQUEST['minSalary']);
 $maxSalary = mysqli_real_escape_string($conn, $_REQUEST['maxSalary']);
 $departmentID = $_SESSION["DEPARTMENT"];
 
-$sql = "SELECT positionID FROM position WHERE departmentID LIKE '$departmentID'";
+
+
+//-------------------------ID generate------------------------------
+//Data Exist?
+$sql = "SELECT count(*) AS total FROM position WHERE departmentID LIKE $departmentID";
 $result = mysqli_query($conn,$sql);
 $row = mysqli_fetch_array($result);
 
-$positionID = 10000000 + (int)$departmentID*1000 + ;
+if($row['total'] === '0')//No
+{
+  $positionID = 10000000 + $departmentID*1000 + 1;
+  $positionID = ''.$positionID;
+  $positionID = substr($positionID, 1);
+}
+else//Yes
+{
+  //After the last ID
+  $sql = "SELECT positionID AS lastID FROM position WHERE departmentID LIKE '$departmentID'";
+  $result = mysqli_query($conn,$sql);
+  $row = mysqli_fetch_array($result);
+
+  $positionID = 10000000 + (int)$row['lastID'] + 1;
+  $positionID = ''.$positionID.'';
+  $positionID = substr($positionID, 1);
+}
+
+$_SESSION["BRANCH"] = $branchName;
+$_SESSION["DEPARTMENT"] = $departmentID;
+
 
 if (isset($_POST['create'])) {
   //create
   $sql = "INSERT INTO position (positionID, positionName, minSalary, maxSalary, departmentID)
-  VALUES ('$departmentID', '$departmentName', '$branchName')";
+  VALUES ('$positionID', '$positionName', '$minSalary', '$maxSalary', '$departmentID')";
 
   if ($conn->query($sql) === TRUE) {
-    $_SESSION["BRANCH"] = $branchName;
-    header('Location: http://localhost/HRPJ/HR/NewDepartment.php');
+    header('Location: http://localhost/HRPJ/HR/PositionandSalary.php');
   }
   else
   {
@@ -44,17 +69,18 @@ else if (isset($_POST['edit']))
 {
   $whichID = $_POST['edit'];
   //edit
-  //currently not doing anything
-  header('Location: http://localhost/HRPJ/HR/NewDepartment.php');
+  //echo "currently not doing anything";
+  header('Location: http://localhost/HRPJ/HR/PositionandSalary.php');
   //end edit
 }
 else if (isset($_POST['delete']))
 {
   //delete
   $whichID = $_POST['delete'];
-  $sql = "DELETE FROM department WHERE departmentID='$whichID'";
+  $sql = "DELETE FROM position WHERE positionID='$whichID'";
   if(mysqli_query($conn, $sql)){
     echo "Record was deleted successfully.";
+    header('Location: http://localhost/HRPJ/HR/PositionandSalary.php');
   }
   else{
     echo "ERROR: Could not able to execute $sql. ". mysqli_error($conn);
@@ -62,12 +88,6 @@ else if (isset($_POST['delete']))
   //end delete
 }
 
-
-
-$_SESSION["BRANCH"] = $branchName;
-$_SESSION["DEPARTMENT"] = $departmentID;
-
-header('Location: http://localhost/HRPJ/HR/NewDepartment.php');
 
 $conn->close();
 
