5b5ffdc33e66d5dabab42f1cb71f0bafd9c1b583
Charoenboon <lazykiller555@hotmail.com>
2019-05-27 20:29:52 +0700

    Update TimeManageSuccessForHR-04.php

--- HR/TimeManageSuccessForHR-04.php
+++ HR/TimeManageSuccessForHR-04.php
@@ -45,7 +45,7 @@ $dm = $_SESSION["result2"];
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 						<a class="dropdown-item" href="http://localhost/HRPJ/HR/InforMeHR.php">Only Me</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HR/SearchInforStaff-01.php">Any Staffs</a>
-						<a class="dropdown-item" href="#">Branch</a>
+						<a class="dropdown-item" href="http://localhost/HRPJ/HR/InforBranch.php">Branch</a>
 					</div>
 				</li>
 				<li class="nav-item active">
