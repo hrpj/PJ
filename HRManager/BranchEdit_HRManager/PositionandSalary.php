272d811c00b347535bd966aec4e542e71e2155ec
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 15:13:06 +0700

    Update PositionandSalary.php

--- HRManager/BranchEdit_HRManager/PositionandSalary.php
+++ HRManager/BranchEdit_HRManager/PositionandSalary.php
@@ -20,7 +20,7 @@ session_start();
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>PositionandSalary</title>
+    <title>Manage Position</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
@@ -119,6 +119,7 @@ session_start();
            $sql = "SELECT * FROM position WHERE departmentID LIKE '$departmentID'";
            $result = mysqli_query($con,$sql);
          ?>
+				 <br><br>
 
         <table class="table">
          <thead class="thead-dark">
