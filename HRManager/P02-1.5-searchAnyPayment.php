8d1d405da6c3c06eed00a5519555ae7797eee882
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-28 21:55:18 +0700

    Update P02-1.5-searchAnyPayment.php

--- HRManager/P02-1.5-searchAnyPayment.php
+++ HRManager/P02-1.5-searchAnyPayment.php
@@ -1,10 +1,3 @@
-<?php     		
-				$result2 = mysqli_query($con,"SELECT * FROM department");
-				while ($row = mysqli_fetch_array($result2))
-				{
-					$departmentID = $row['departmentID'];
-					$departmentName = $row['departmentName'];
-					$BranchName2 = $row['BranchName'];
-					echo "<option value=".$departmentID.">".$departmentName."-".$BranchName."</option>";
-				}
-?>
+<?php
+ echo "321";
+ ?>
