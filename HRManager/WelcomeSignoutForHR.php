2769650449fb3371dfb5e2962529d388f65a1aae
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 20:22:44 +0700

    Update WelcomeSignoutForHR.php

--- HRManager/WelcomeSignoutForHR.php
+++ HRManager/WelcomeSignoutForHR.php
@@ -69,7 +69,7 @@ session_start();
 						<a class="dropdown-item" href="#">New Staff</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/NewBranchForHR.html">New Branch</a>
 						<a class="dropdown-item" href="NewBranchForHR.html">New Training Course</a>
-						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/NewBranch1.html">Edit Branch</a>
+						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/EditBranch1.php">Edit Branch</a>
 						<a class="dropdown-item" href="NewTraining.html">Delete Staff</a>
 					</div>
 				</li>
