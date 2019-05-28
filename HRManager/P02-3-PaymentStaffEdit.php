8dd79018c2d5b9f1c737045d752d39cc6435d69c
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-29 02:41:07 +0700

    Update P02-3-PaymentStaffEdit.php

--- HRManager/P02-3-PaymentStaffEdit.php
+++ HRManager/P02-3-PaymentStaffEdit.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+	$con=mysqli_connect("localhost","root","","hrmanager");
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -85,72 +94,105 @@
 		</div>
 	</nav>
 </head>
-
-  <body>
-      <div class = "Payment"><h3>Payment Slip</h></div>
-      <div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
-      <!-- Information -->
-      <div class="Infor"><i class="fas fa-address-card"></i>Staff ID : HR031003<br><br><i class="fas fa-layer-group"></i>Department : Human Resource Management<br><br><i class="fas fa-calendar-alt"></i>Month : DEC</div>
-      <div class="Infor2"><br><br><i class="fas fa-briefcase"></i>Position : Manager<br><br><i class="fas fa-map-marker-alt"></i></i>Branch : Bang Mod<br><br><i class="fas fa-history"></i>Year : 2018</div>
-      <!-- End Information -->
-      <div class="Salary"><h5>Salary : <div class="SalaryFill"><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Salary"></h></div></div>
-          <div class ="SaveSalary"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
-      <form>
-          <div class="table1">
-              Bonus
-      <table class="table">
-      <thead class="thead-dark">
-      <tr>
-      <th scope="col">Type</th>
-      <th scope="col">Amount</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Type"></td>
-      <td><input type="text" class="form-control" id="Amount" aria-describedby="Amount" placeholder="Original Amount"></td>
-      </tr>
-      <tr>
-      <td><input type="text" class="form-control" id="Type" aria-describedby="Type"></td>
-      <td><input type="text" class="form-control" id="Amount" aria-describedby="Amount"></td>
-      </tr>
-      </tbody>
-      </table>
-      <div class="table2">
-          Deduction
-      <table class="table">
-      <thead class="thead-dark">
-      <tr>
-      <th scope="col">Type</th>
-      <th scope="col">Amount</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Type"></td>
-      <td><input type="text" class="form-control" id="Amount" aria-describedby="Amount" placeholder="Original Amount"></td>
-      </tr>
-      <tr>
-      <td><input type="text" class="form-control" id="Type" aria-describedby="Type"></td>
-      <td><input type="text" class="form-control" id="Amount" aria-describedby="Amount"></td>
-      </tr>
-      </tbody>
-      </table>
-  </div>
-</form>
-<div class ="SaveBonus"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
-<div class ="AddBonus"><a href="BranchToDepartment.html" class="button-link">Add</a></div>
-<div class ="SaveDeduc"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
-<div class ="AddDeduc"><a href="BranchToDepartment.html" class="button-link">Add</a></div>
-      <table class="Back">
-          <tr><td>
-      <button type="button" class="btn btn-dark" onclick="window.location.href = 'ListOfBill.html';">Back</button>
-      </td></tr>
-      </table>
+<?php       
+			$search = $_SESSION["FoundID2"];
+			$date = $_SESSION["date"];
+			$type = $_SESSION["type"];
+			$result = mysqli_query($con,"SELECT * FROM staff WHERE staffID LIKE '$id'");
+            while ($row = mysqli_fetch_array($result))
+            {
+                $positionID = $row['positionID'];
+            }
+            $result = mysqli_query($con,"SELECT * FROM position WHERE positionID LIKE '$positionID'");
+            while ($row = mysqli_fetch_array($result))
+            {
+				$positionName = $row['positionName'];
+                $departmentID = $row['departmentID'];
+            }
+            $result = mysqli_query($con,"SELECT * FROM department WHERE departmentID LIKE '$departmentID'");
+            while ($row = mysqli_fetch_array($result))
+            {
+                $departmentName = $row['departmentName'];
+				$branchName = $row['BranchName'];
+            }			
+?>
+<body>
+    <div class = "Payment"><h3>Payment Slip</h></div>
+    <div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
+	
+    <!-- Information -->
+    <div class="Infor"><i class="fas fa-address-card"></i>Staff ID : HR031003<br>
+		<br><i class="fas fa-layer-group"></i>Department : Human Resource Management<br>
+		<br><i class="fas fa-calendar-alt"></i>Month : DEC
+	</div>
+    <div class="Infor2"><br><br><i class="fas fa-briefcase"></i>Position : Manager<br>
+		<br><i class="fas fa-map-marker-alt"></i></i>Branch : Bang Mod<br>
+		<br><i class="fas fa-history"></i>Year : 2018
+	</div>
+	
+    <!-- End Information -->
+    <div class="Salary"><h5>Salary : <div class="SalaryFill"><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Salary"></h></div></div>
+    <div class ="SaveSalary"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
+    <form>
+		<div class="table1">
+			Bonus
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">Type</th>
+					<th scope="col">Amount</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Type"></td>
+					<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount" placeholder="Original Amount"></td>
+				</tr>
+				<tr>
+					<td><input type="text" class="form-control" id="Type" aria-describedby="Type"></td>
+					<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount"></td>
+				</tr>
+			</tbody>
+		</table>
+		
+		<div class="table2">
+			Deduction
+			<table class="table">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">Type</th>
+						<th scope="col">Amount</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Type"></td>
+						<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount" placeholder="Original Amount"></td>
+					</tr>
+					<tr>
+						<td><input type="text" class="form-control" id="Type" aria-describedby="Type"></td>
+						<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</form>
+	
+	<div class ="SaveBonus"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
+	<div class ="AddBonus"><a href="BranchToDepartment.html" class="button-link">Add</a></div>
+	<div class ="SaveDeduc"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
+	<div class ="AddDeduc"><a href="BranchToDepartment.html" class="button-link">Add</a></div>
+    <table class="Back">
+		<tr><td>
+			<button type="button" class="btn btn-dark" onclick="window.location.href = 'ListOfBill.html';">Back</button>
+		</td></tr>
+    </table>
+	
+	
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+	</body>
 </html>
