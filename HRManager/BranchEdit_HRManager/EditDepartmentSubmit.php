4f8310b5fecaa4e231d84f22308655c1426e38a1
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 14:44:01 +0700

    Update EditDepartmentSubmit.php

--- HRManager/BranchEdit_HRManager/EditDepartmentSubmit.php
+++ HRManager/BranchEdit_HRManager/EditDepartmentSubmit.php
@@ -20,7 +20,7 @@ $departmentID = $_SESSION["DEPARTMENT"];
 $sql = "UPDATE department SET departmentName='$departmentName' WHERE departmentID = $departmentID";
 
 if ($conn->query($sql) === TRUE) {
-    header('Location: http://localhost/HRPJ/HRManager/BranchEdit_HRManager/NewDepartment.php');
+    header('Location: http://localhost/HRPJ/HRManager/BranchEdit_HRManager/PositionEditDepartment.php');
 }
 else {
     echo "Error updating record: " . $conn->error;
