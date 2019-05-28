25fb81a3a6de36cd0de36d3f59a1c692ae0f241f
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 12:38:00 +0700

    Update search.php

--- HRManager/search.php
+++ HRManager/search.php
@@ -2,36 +2,36 @@
 session_start();
 	$con=mysqli_connect("localhost","root","","hrmanager");
 	// Check connection
-	if (mysqli_connect_errno()) 
+	if (mysqli_connect_errno())
 	{
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	
+
 	// escape variables for security
 	$id = mysqli_real_escape_string($con, $_POST['id']);
 	$search = mysqli_real_escape_string($con, $_POST['search']);
-	
-	
+
+
 	$result = mysqli_query($con,"SELECT staffID FROM staff WHERE staffId LIKE '$search'");
 	$count=$result->num_rows;
-	if ((empty($count))) 
+	if ((empty($count)))
 	{
-		header("Location: http://localhost/HRPJ/HR/TimeAttendanceSearchForHR-01ERROR.php");
+		header("Location: http://localhost/HRPJ/HRManager/TimeAttendanceSearchForHR-01ERROR.php");
 	}
-	
+
 	while ($row = mysqli_fetch_array($result))
 	{
 		echo $row['staffID'];
 		$_SESSION["search"] = $row['staffID'];
 		if ($result != False )
 		{
-			header("Location: http://localhost/HRPJ/HR/TimeAttendanceSearchSuccessForHR-02.php");
+			header("Location: http://localhost/HRPJ/HRManager/TimeAttendanceSearchSuccessForHR-02.php");
 		}
 		else
 		{
-			header("Location: http://localhost/HRPJ/HR/TimeAttendanceSearchForHR-01ERROR.php");
+			header("Location: http://localhost/HRPJ/HRManager/TimeAttendanceSearchForHR-01ERROR.php");
 		}
-	} 
+	}
 
 	mysqli_close($con);
 ?>
