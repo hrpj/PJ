f998bdda48f7de1f0142a1bcfef6ac1776133ca7
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-28 20:26:16 +0700

    Update P01-1-PaymentSearchForHR.php

--- HRManager/P01-1-PaymentSearchForHR.php
+++ HRManager/P01-1-PaymentSearchForHR.php
@@ -44,15 +44,9 @@ session_start();
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/InforBranch.php">Branch</a>
 					</div>
 				</li>
-				<li class="nav-item dropdown">
-		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          Time Attendance
-		        </a>
-		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		              <a class="dropdown-item" href="http://localhost/HRPJ/HRManager/TimeAttendanceSearchForHR-01.php">Daily Attendance Status</a>
-		              <a class="dropdown-item" href="#">Leave</a>
-		        </div>
-		    </li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/HRPJ/HRManager/TimeAttendanceSearchForHR-01.php">Time Attendance</a>
+				</li>
 				<li class="nav-item dropdown active">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						Payment Slip
@@ -69,9 +63,8 @@ session_start();
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 						<a class="dropdown-item" href="#">New Staff</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/NewBranchForHR.html">New Branch</a>
-						<a class="dropdown-item" href="NewBranchForHR.html">New Training Course</a>
-				        <a class="dropdown-item" href="NewDepartment.html">Edit Branch</a>
-				        <a class="dropdown-item" href="NewTraining.html">Delete Staff</a>
+						<a class="dropdown-item" href="#">New Department</a>
+						<a class="dropdown-item" href="#">New Training Course</a>
 					</div>
 				</li>
 				<li class="nav-item dropdown">
@@ -118,7 +111,7 @@ session_start();
             {
                 $departmentName = $row['departmentName'];
 				$branchName = $row['BranchName'];
-            }
+            }			
 ?>
 <body>
 	<form action="searchMyPaymant.php" method="POST">
