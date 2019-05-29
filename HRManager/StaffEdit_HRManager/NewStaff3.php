1651392030b67dc2404780da1b53ae7268d06cfb
Preeyada <tune_jantawong@hotmail.com>
2019-05-29 16:08:43 +0700

    Update NewStaff3.php

--- HRManager/StaffEdit_HRManager/NewStaff3.php
+++ HRManager/StaffEdit_HRManager/NewStaff3.php
@@ -56,7 +56,7 @@ session_start();
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="stylenewstaff.css" rel="stylesheet">
+    <link href="stylenewstaff3.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display&display=swap" rel="stylesheet">
 
     <!-- Nav Bar -->
