0c994e90598295038f0a9e55cccc63b72216de3d
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-30 04:03:27 +0700

    Update WelcomeSignoutForHR.php

--- HRManager/WelcomeSignoutForHR.php
+++ HRManager/WelcomeSignoutForHR.php
@@ -7,6 +7,8 @@ session_start();
 	{
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
+	
+	unset ($_SESSION["courseID"]);
 ?>
 <html lang="en">
 <head>
@@ -58,8 +60,8 @@ session_start();
 						Payment Slip
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/P01-1-PaymentSearchForHR.php">Only Me</a>
-						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/P02-1-PaymentStaffSearch.php">Any Staffs</a>
+						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/P01-1-PaymentSearchForHR.php">Only Me</a>			
+						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/P02-1-PaymentStaffSearch.php">Any Staffs</a>		
 					</div>
 				</li>
 				<li class="nav-item dropdown">
@@ -70,7 +72,7 @@ session_start();
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/StaffEdit_HRManager/NewStaff1.php">New Staff</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/NewBranchForHR.html">New Branch</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/T01-1-NewTraining_1.php">New Training Course</a>
-						<a class="dropdown-item" href="NewTraining.html">Edit Training Course</a>
+						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/T03-1-Edittraining.php">Edit Training Course</a>
 						<a class="dropdown-item" href="http://localhost/HRPJ/HRManager/BranchEdit_HRManager/EditBranch1.php">Edit Branch</a>
 						<a class="dropdown-item" href="NewBranchForHR.html">Edit Staff</a>
 						<a class="dropdown-item" href="NewTraining.html">Delete Staff</a>
@@ -104,7 +106,6 @@ session_start();
 		</div>
 	</nav>
 </head>
-
 
   <body>
       <br>
