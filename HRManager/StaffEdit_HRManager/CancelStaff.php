94236fd6f67ea486fdec042531212d80d878a6b6
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-30 00:41:15 +0700

    Update CancelStaff.php

--- HRManager/StaffEdit_HRManager/CancelStaff.php
+++ HRManager/StaffEdit_HRManager/CancelStaff.php
@@ -15,18 +15,20 @@ if ($conn->connect_error) {
 
 // Escape user inputs for security
 $staffID = $_SESSION["CR_STAFFID"];
+unset($_SESSION['CR_STAFFID']);
 
-//Delete
-$sql = "DELETE FROM staff WHERE staffID='$staffID'";
+if (isset($_POST['Cancel'])) {
+  //Delete
+  $sql = "DELETE FROM staff WHERE staffID='$staffID'";
 
 
-if ($conn->query($sql) === TRUE) {
-  header('Location: http://localhost/HRPJ/HRManager/WelcomeSignoutForHR.php');
-} else {
-  echo "Something went wrong, can't delete";
-  //header('Location: http://localhost/HRPJ/HRManager/WelcomeSignoutForHR.php');
+  if ($conn->query($sql) === TRUE) {
+    header('Location: http://localhost/HRPJ/HRManager/WelcomeSignoutForHR.php');
+  } else {
+    echo "Something went wrong, can't delete";
+    //header('Location: http://localhost/HRPJ/HRManager/WelcomeSignoutForHR.php');
+  }
 }
-
 $conn->close();
 
 ?>
