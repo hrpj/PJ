084be582f76282b25bfde05401780526cb2bb928
Charoenboon <lazykiller555@hotmail.com>
2019-05-30 03:16:56 +0700

    Create DeleteConcernAction.php

---
+++ HRManager/DeleteConcernAction.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+	$con=mysqli_connect("localhost","root","","hrmanager");
+// Check connection
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	$id = mysqli_real_escape_string($con, $_POST['id']);
+	$concernDelete = mysqli_real_escape_string($con, $_POST['concernDelete']);
+
+//check if no input found
+	if (empty($concernDelete))
+	{
+		echo("ERROR!!! : No data input found.");
+	}
+	if (empty($id))
+	{
+		echo("ERROR!!! : No ID found.");
+	}
+
+	$result = mysqli_query($con,"DELETE FROM concern WHERE staffID LIKE '$id' AND concernBehavor LIKE '$concernDelete';");
+
+	if (empty($result))
+		echo "ERROR!!! : No result returned.";
+	else
+		echo "Deleted successfully.";
+	header("Location:http://localhost/HRPJ/HRManager/CompetenceAddForHR.php?id=$id");
+?>
