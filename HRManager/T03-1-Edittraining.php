31039ab01137c6d5fd25bc75a9c05b661f9f3468
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-30 03:10:04 +0700

    Update T03-1-Edittraining.php

--- HRManager/T03-1-Edittraining.php
+++ HRManager/T03-1-Edittraining.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+	$con=mysqli_connect("localhost","root","","hrmanager");
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+?>
 <!doctype html>
 <html lang="en">
   <head>
