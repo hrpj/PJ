25ab9c4d94c6fb2d4bd357d29ee221df471257f8
Preeyada <tune_jantawong@hotmail.com>
2019-05-29 17:32:50 +0700

    Update P02-3-PaymentStaffEdit.php

--- HRManager/P02-3-PaymentStaffEdit.php
+++ HRManager/P02-3-PaymentStaffEdit.php
@@ -95,15 +95,15 @@ session_start();
 		</div>
 	</nav>
 </head>
-<?php       
+<?php
 			$search = $_SESSION["FoundID2"];
 			$date = $_SESSION["date"];
 			$type = $_SESSION["type"];
-			
+
 			$day = explode("-", $date);
 			$year = $day[0];
 			$month = $day[1] . '-'. $day[2];
-			
+
 			$result = mysqli_query($con,"SELECT * FROM staff WHERE staffID LIKE '$search'");
             while ($row = mysqli_fetch_array($result))
             {
@@ -120,12 +120,12 @@ session_start();
             {
                 $departmentName = $row['departmentName'];
 				$branchName = $row['BranchName'];
-            }			
+            }
 ?>
 <body>
     <div class = "Payment"><h3>Payment Slip</h></div>
     <div align ="center"><img src="IMG_1543.jpg" width="400" height="300"></div>
-	
+
     <!-- Information -->
     <div class="Infor"><i class="fas fa-address-card"></i>Staff ID : <?php echo $search ; ?><br>
 		<br><i class="fas fa-layer-group"></i>Department : <?php echo $departmentName ; ?><br>
@@ -135,7 +135,7 @@ session_start();
 		<br><i class="fas fa-map-marker-alt"></i></i>Branch :<?php echo $branchName ; ?><br>
 		<br><i class="fas fa-history"></i>Year : <?php echo $year ; ?>
 	</div>
-	
+
 <?php	$result = mysqli_query($con,"SELECT * FROM increasesalaryrecord WHERE staffID LIKE '$search' AND year LIKE '$year' And date < '$month%' Order by date Desc");
 
 		$count=$result->num_rows;
@@ -155,7 +155,7 @@ session_start();
 ?>
     <!-- End Information -->
 	<form action="P02-3.25-editSalary.php" method="POST">
-		<div class="Salary"><h5>Salary : 
+		<div class="Salary"><h5>Salary :
 			<div class="SalaryFill">
 				<input type='hidden' name='month' value=".<?php echo $month ; ?>.">
 				<input type='hidden' name='year' value=".<?php echo $year ; ?>.">
@@ -169,7 +169,7 @@ session_start();
 		</div>
 	</form>
     <form>
-	
+
 		<div class="table1">
 			Bonus
 		<table class="table">
@@ -190,30 +190,8 @@ session_start();
 				</tr>
 			</tbody>
 		</table>
-		
-		<div class="table2">
-			Deduction
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">Type</th>
-						<th scope="col">Amount</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td><input type="text" class="form-control" id="Type" aria-describedby="Type" placeholder="Original Type"></td>
-						<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount" placeholder="Original Amount"></td>
-					</tr>
-					<tr>
-						<td><input type="text" class="form-control" id="Type" aria-describedby="Type"></td>
-						<td><input type="text" class="form-control" id="Amount" aria-describedby="Amount"></td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
 	</form>
-	
+
 	<div class ="SaveBonus"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
 	<div class ="AddBonus"><a href="BranchToDepartment.html" class="button-link">Add</a></div>
 	<div class ="SaveDeduc"><a href="BranchToDepartment.html" class="button-link">Save</a></div>
@@ -223,8 +201,8 @@ session_start();
 			<button type="button" class="btn btn-dark" onclick="window.location.href = 'ListOfBill.html';">Back</button>
 		</td></tr>
     </table>
-	
-	
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
