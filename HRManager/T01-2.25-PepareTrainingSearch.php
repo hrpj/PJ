3ffc42ce84144b130007f3e60fc0ab936085fd93
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-30 01:00:50 +0700

    Update T01-2.25-PepareTrainingSearch.php

--- HRManager/T01-2.25-PepareTrainingSearch.php
+++ HRManager/T01-2.25-PepareTrainingSearch.php
@@ -1,2 +1,17 @@
-
+<?php
+session_start();
+	$con=mysqli_connect("localhost","root","","hrmanager");
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	
+	$departmentID = mysqli_real_escape_string($con, $_POST['departmentID']);
+	
+	$_SESSION['departmentID'] = $departmentID;
+	header("Location: http://localhost/HRPJ/HRManager/T01-2-NewTraining_2.php");
+	
+	mysqli_close($con);
+?>
 
