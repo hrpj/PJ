5c7e7c9c7c6a9cc26203caf37a3ae6700d3fcf92
Preeyada <tune_jantawong@hotmail.com>
2019-05-29 20:30:41 +0700

    Update WorkHistory.php

--- HRManager/StaffEdit_HRManager/WorkHistory.php
+++ HRManager/StaffEdit_HRManager/WorkHistory.php
@@ -26,7 +26,7 @@ $staffID = $_SESSION["CR_STAFFID"]
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="stylenewstaff.css" rel="stylesheet">
+    <link href="styleworkhis.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Playfair+Display&display=swap" rel="stylesheet">
 
     <!-- Nav Bar -->
