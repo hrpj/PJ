fb4a08ed67ef99433c22903314af9552adb1fd10
NattapatSeang <51043893+NattapatSeang@users.noreply.github.com>
2019-05-29 15:11:59 +0700

    Update CreateStaffController.php

--- HRManager/StaffEdit_HRManager/CreateStaffController.php
+++ HRManager/StaffEdit_HRManager/CreateStaffController.php
@@ -13,8 +13,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_POST['Submit'] === 'SubmitBName') {
-  //_________Form1____________
+if ($_POST["Submit"] === 'SubmitBName') {
+  //___________________________________________________Form1__________________________________________________________
   $_SESSION["STARTDATE"] = mysqli_real_escape_string($conn, $_REQUEST['startDate']);
   $_SESSION["FNAME"] = mysqli_real_escape_string($conn, $_REQUEST['fName']);
   $_SESSION["LNAME"] = mysqli_real_escape_string($conn, $_REQUEST['lName']);
@@ -26,8 +26,8 @@ if ($_POST['Submit'] === 'SubmitBName') {
   $_SESSION["BRANCHNAME"] = $_POST['branchName'];
   header('Location: http://localhost/HRPJ/HRManager/StaffEdit_HRManager/NewStaff2.php');
 }
-else if ($_POST['Submit'] === 'SubmitDName') {
-  //________Form2_______________
+else if ($_POST["Submit"] === 'SubmitDName') {
+  //_________________________________________________________Form2_____________________________________________
   $_SESSION["STARTDATE"] = mysqli_real_escape_string($conn, $_REQUEST['startDate']);
   $_SESSION["FNAME"] = mysqli_real_escape_string($conn, $_REQUEST['fName']);
   $_SESSION["LNAME"] = mysqli_real_escape_string($conn, $_REQUEST['lName']);
@@ -40,27 +40,51 @@ else if ($_POST['Submit'] === 'SubmitDName') {
   $_SESSION["DEPARTMENT"] = $_POST['departmentID'];
   header('Location: http://localhost/HRPJ/HRManager/StaffEdit_HRManager/NewStaff3.php');
 }
-else if ($_POST['Submit'] === 'Submit') {
-  //_______________Form3_______________
-  $_SESSION["STARTDATE"] = mysqli_real_escape_string($conn, $_REQUEST['startDate']);
-  $_SESSION["FNAME"] = mysqli_real_escape_string($conn, $_REQUEST['fName']);
-  $_SESSION["LNAME"] = mysqli_real_escape_string($conn, $_REQUEST['lName']);
-  $_SESSION["ADDRESS"] = mysqli_real_escape_string($conn, $_REQUEST['address']);
-  $_SESSION["TELNO"] = mysqli_real_escape_string($conn, $_REQUEST['telNo']);
-  $_SESSION["DATEOFBIRTH"] = mysqli_real_escape_string($conn, $_REQUEST['dateOfBirth']);
-  $_SESSION["GENDER"] = $_POST['gender'];  // Storing Selected Value In Variable
-  $_SESSION["BANKACCOUNT"] = mysqli_real_escape_string($conn, $_REQUEST['bankAccount']);
-  $_SESSION["BRANCHNAME"] = $_POST['branchName'];
-  $_SESSION["DEPARTMENT"] = $_POST['departmentID'];
-  $_SESSION["POSITION"] = $_POST['positionID'];
+else if ($_POST["Submit"] === 'Submit') {
+  //___________________________________________________Form3_______________________________________________________________
 
+  $startDate = mysqli_real_escape_string($conn, $_REQUEST['startDate']);
+  $fName = mysqli_real_escape_string($conn, $_REQUEST['fName']);
+  $lName = mysqli_real_escape_string($conn, $_REQUEST['lName']);
+  $address = mysqli_real_escape_string($conn, $_REQUEST['address']);
+  $telNo = mysqli_real_escape_string($conn, $_REQUEST['telNo']);
+  $dateOfBirth = mysqli_real_escape_string($conn, $_REQUEST['dateOfBirth']);
+  $gender = $_POST['gender'];  // Storing Selected Value In Variable
+  $bankAccount = mysqli_real_escape_string($conn, $_REQUEST['bankAccount']);
+  $positionID = $_POST['positionID'];
+  $password = $_POST['password'];
 
+  //___________________________________________  Generate staffID _________________________ _______________
+
+  $sql = "SELECT count(*) AS total FROM staff WHERE positionID LIKE $positionID";
+  $result = mysqli_query($conn,$sql);
+  $row = mysqli_fetch_array($result);
+
+  if($row['total'] === '0')//No
+  {
+    $staffID = 1000000000000 + (int)$positionID*100000 + 1;
+    $staffID = ''.$staffID;
+    $staffID = substr($staffID, 1);
+  }
+  else//Yes
+  {
+    //After the last ID
+    $sql = "SELECT MAX(staffID) AS lastID FROM staff WHERE positionID LIKE '$positionID'";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result);
+
+    $staffID = 1000000000000 + (int)$row['lastID'] + 1;
+    $staffID = ''.$staffID.'';
+    $staffID = substr($staffID, 1);
+  }
+
+  //___________________________________________________Insert Picture__________________________________________________
   $ext = pathinfo(basename($_FILES['picture']['name']), PATHINFO_EXTENSION);
-  //$newImageName
+  $newImageName = 'img_'.$uniqueid().'.'.$ext;
 
-  echo "SUCCESS";
+  echo "SUCCESS".$staffID;
 
-  //echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
+  echo "<a href=\"javascript:history.go(-1)\">GO BACK</a>";
 }
 
 // Escape user inputs for security
