3c1520b721f40666c62d55997f3eb73671cd3cd4
julopa32 <51020721+julopa32@users.noreply.github.com>
2019-05-29 16:49:47 +0700

    Update P02-2.5-EditBill.php

--- HRManager/P02-2.5-EditBill.php
+++ HRManager/P02-2.5-EditBill.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
+	$con=mysqli_connect("localhost","root","","hrmanager");
+	// Check connection
+	if (mysqli_connect_errno())
+	{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 
 	$search = mysqli_real_escape_string($con, $_POST['edit']);
 	$date = mysqli_real_escape_string($con, $_POST['edit2']);
 	$type = mysqli_real_escape_string($con, $_POST['edit3']);
 		
 	$_SESSION["FoundID2"] = $search;
-	$_SESSION["date"] = $positionID;
-	$_SESSION["type"] = $departmentID;
+	$_SESSION["date"] = $date;
+	$_SESSION["type"] = $type;
 	
 	header("Location: http://localhost/HRPJ/HRManager/P02-3-PaymentStaffEdit.php");
+	mysqli_close($con);
 ?>
