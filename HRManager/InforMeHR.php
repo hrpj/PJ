d011d9cfb2b1e538784ca3a729019befadf87cfb
Charoenboon <lazykiller555@hotmail.com>
2019-05-29 19:06:32 +0700

    Update InforMeHR.php

--- HRManager/InforMeHR.php
+++ HRManager/InforMeHR.php
@@ -148,7 +148,7 @@
         <br><i class="fas fa-money-check-alt"></i>Bank Account : <?php echo "$bankAccount"; ?>
     </div>
         <!-- End Information -->
-    <div class="Training">Check Training Course : <a href="TrainingCourse.html" class="button-link">Click here!</a></div>
+    <div class="Training">Check Training Course : <a href="http://localhost/HRPJ/HRManager/TrainingCourse.php" class="button-link">Click here!</a></div>
 
     <!-- Table Work History -->
     <div class = "WorkHis"><h4>Work History</h></div>
